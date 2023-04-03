356cdc577bdc619a3236a6dbb3e306d8e1edf60a
amaresh sahoo <eb.amareshsahoo@gmail.com>
2023-04-03 16:08:55 +0530

    update

--- contracts/fullpresale.sol
+++ contracts/fullpresale.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.9.0;
 
-import "@openzeppelin/contracts@4.8.2/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 contract vesting {
 
     struct preSalePhase {
@@ -26,15 +26,16 @@ contract vesting {
     }
 
     IERC20 public token;
-    uint256 public TGE;
+    uint256 public TGE = block.timestamp;
     address public owner; 
     uint public id ;
-    uint256 [] public category = [1 , 2 , 3 , 4 , 5] ;
+    uint public cat;
+   
     
     
     mapping (uint256 => preSalePhase) preSaleNumber;
 
-    mapping(uint256 => mapping (address => preSaleInvestor)) public preSaleInvestorList;
+    mapping(uint256  => mapping ( uint =>mapping (address => preSaleInvestor))) public preSaleInvestorList;
 
     constructor (address _token) {
         owner = msg.sender;
@@ -60,66 +61,72 @@ contract vesting {
         return true;
     }
 
+    function category(uint _amount) public pure returns(uint256){
+        
+        uint cate;
+        if(_amount> 100 && _amount< 500){        
+            cate = 1 ;
+        }
+        else if (_amount> 500 && _amount< 1000){        
+            cate = 2;
+        }
+        else if (_amount> 1000 && _amount< 5000){        
+            cate = 3;
+        }
+        else if (_amount> 5000 && _amount< 10000){        
+            cate = 4;
+        }
+        else if (_amount> 100000 && _amount< 10000){        
+            cate = 5;
+        }
+
+        return cate;
+    }
+
     function lock(uint256 _id ,address _from , address _investor , uint256 _amount) external {
         require(_amount <= preSaleNumber[id].totalTokens , "Insufficient tokens try a lower value");
         require(block.timestamp > preSaleNumber[id].startTime , "Time of presale has not yet arrived");
         require(block.timestamp > preSaleNumber[id].endTime , "Time of presale has passed");
         
+        cat = category(_amount);
+
         token.transferFrom(_from, address(this), _amount);
-        preSaleInvestorList[_id][_investor].balance = _amount;
-        preSaleInvestorList[_id][_investor].invested = true;
-        preSaleInvestorList[_id][_investor].locked = true;
-        preSaleInvestorList[_id][_investor].claimed = false;
-        preSaleInvestorList[_id][_investor].lockTime = TGE;
-        preSaleInvestorList[_id][_investor].refferal = keccak256(abi.encodePacked(_investor)); 
-        
-        if(_amount> 100 && _amount< 500){        
-            preSaleInvestorList[_id][_investor].category = category[0];
-        }
-        else if (_amount> 500 && _amount< 1000){        
-            preSaleInvestorList[_id][_investor].category = category[1];
-        }
-        else if (_amount> 1000 && _amount< 5000){        
-            preSaleInvestorList[_id][_investor].category = category[2];
-        }
-        else if (_amount> 5000 && _amount< 10000){        
-            preSaleInvestorList[_id][_investor].category = category[3];
-        }
-        else if (_amount> 100000 && _amount< 10000){        
-            preSaleInvestorList[_id][_investor].category = category[4];
-        }
-
-
+        preSaleInvestorList[_id][cat][_investor].balance = _amount;
+        preSaleInvestorList[_id][cat][_investor].invested = true;
+        preSaleInvestorList[_id][cat][_investor].locked = true;
+        preSaleInvestorList[_id][cat][_investor].claimed = false;
+        preSaleInvestorList[_id][cat][_investor].lockTime = TGE;
+        preSaleInvestorList[_id][cat][_investor].refferal = keccak256(abi.encodePacked(_investor)); 
     }
 
-    function unlockedTokens(uint _id , address _investor) internal returns (uint) {
+    function unlockedTokens(uint _id , uint _cat , address _investor) public returns (uint256) {
 
         if(block.timestamp> TGE && block.timestamp< TGE+10){
-            preSaleInvestorList[_id][_investor].unlockedTokens += preSaleInvestorList[_id][_investor].balance/2;
+            preSaleInvestorList[_id][_cat][_investor].unlockedTokens += preSaleInvestorList[_id][_cat][_investor].balance/2;
         }
         if(block.timestamp> TGE+10 && block.timestamp< TGE+20){
-            preSaleInvestorList[_id][_investor].unlockedTokens += preSaleInvestorList[_id][_investor].balance/2;
+            preSaleInvestorList[_id][_cat][_investor].unlockedTokens += preSaleInvestorList[_id][_cat][_investor].balance/2;
         }
 
-        return preSaleInvestorList[_id][_investor].unlockedTokens;
+        return preSaleInvestorList[_id][_cat][_investor].unlockedTokens;
 
     }
 
-    function withdraw(uint _id , address _investor , uint256 _claimAmount) external {
-        require(preSaleInvestorList[_id][_investor].invested , "You are not an investor");
-        require(block.timestamp > preSaleInvestorList[_id][_investor].lockTime ,"Tokens have not been unlocked");
+    function withdraw(uint _id , address _investor , uint256 _cat , uint256 _claimAmount) external {
+        require(preSaleInvestorList[_id][_cat][_investor].invested , "You are not an investor");
+        require(block.timestamp > preSaleInvestorList[_id][_cat][_investor].lockTime ,"Tokens have not been unlocked");
         
-        preSaleInvestorList[_id][_investor].unlockedTokens = unlockedTokens(_id , _investor);
-        if(preSaleInvestorList[_id][_investor].claimed){
-            preSaleInvestorList[_id][_investor].availableForClaim = preSaleInvestorList[_id][_investor].unlockedTokens - preSaleInvestorList[_id][_investor].tokenClaimed;
+        preSaleInvestorList[_id][_cat][_investor].unlockedTokens = unlockedTokens(_id , _cat , _investor);
+        if(preSaleInvestorList[_id][_cat][_investor].claimed){
+            preSaleInvestorList[_id][_cat][_investor].availableForClaim = preSaleInvestorList[_id][_cat][_investor].unlockedTokens - preSaleInvestorList[_id][_cat][_investor].tokenClaimed;
         }
         else{
-            preSaleInvestorList[_id][_investor].availableForClaim = preSaleInvestorList[_id][_investor].unlockedTokens;
+            preSaleInvestorList[_id][_cat][_investor].availableForClaim = preSaleInvestorList[_id][_cat][_investor].unlockedTokens;
         }
 
-        if(_claimAmount<= preSaleInvestorList[_id][_investor].availableForClaim) {
-            preSaleInvestorList[id][_investor].claimed = true;
-            preSaleInvestorList[_id][_investor].locked = false;
+        if(_claimAmount<= preSaleInvestorList[_id][_cat][_investor].availableForClaim) {
+            preSaleInvestorList[id][_cat][_investor].claimed = true;
+            preSaleInvestorList[_id][_cat][_investor].locked = false;
             token.transfer(_investor , _claimAmount);
         }
         
